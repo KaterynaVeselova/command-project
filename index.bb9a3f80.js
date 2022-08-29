1ec9f7cca069aeaba021345ad832171aa3fe123f
KaterynaVeselova <107804176+KaterynaVeselova@users.noreply.github.com>
2022-08-29 23:02:34 +0000

    Deploying to gh-pages from @ KaterynaVeselova/command-project@7aeeee220cc4e2c0072167c4d177e8d30e9f7987 🚀

---
+++ index.bb9a3f80.js
@@ -0,0 +1,2 @@
+(()=>{const e={openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]"),body:document.querySelector("[data-page]")};function o(){e.modal.classList.toggle("is-hidden"),e.body.classList.toggle("no-scroll")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)})();
+//# sourceMappingURL=index.bb9a3f80.js.map
